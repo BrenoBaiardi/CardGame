af69e2db9bb4b2f8ee7923832ea6631549718a7b
Breno César Baiardi <brenocbo@gmail.com>
2020-02-29 11:31:09 -0300

    trying to fiz git problems

---
+++ front2/js/init_arena.js
@@ -0,0 +1,23 @@
+function montaTr(size,row_number){
+  var arenaTr = document.createElement("tr")
+  arenaTr.classList.add("paciente");
+
+    for (var col_number = 1; col_number < size+1; col_number++) {
+        arenaTr.appendChild(montaTd(String(row_number).concat("x",col_number),"ArenaCell"));
+    }
+  return arenaTr;
+}
+
+function montaTd(dado, classe){
+  var td = document.createElement("td");
+  td.textContent = dado;
+  td.classList.add(classe);
+  return td;
+}
+
+var tabela = document.querySelector("#arena-table");
+
+for (var i = 1; i <= 5; i++) {
+  var arenaTr = montaTr(5,i);
+  tabela.appendChild(arenaTr);
+}
